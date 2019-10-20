0c0598babfb60dd5f2671a350c0f5b31c15f0915
farrukh-e <46006626+farrukh-e@users.noreply.github.com>
2019-10-19 23:02:25 -0400

    Add files via upload

--- data_aqusition/src/CAN_node.py
+++ data_aqusition/src/CAN_node.py
@@ -12,7 +12,7 @@ class pyCAN:
 	
 	def __init__(self):
 		
-		self.channel = 'can0'
+		self.channel = 'vcan0'
 		self.bustype = 'socketcan'
 		self.bitrate = 1000000 
 
@@ -21,50 +21,49 @@ class pyCAN:
 		self.out = [0,0,0,0]
 		rospy.init_node('driver', anonymous=True)
 		#can_pub =  rospy.Publisher("can_bus", Int16, queue_size = 100)
+		#Decimal to hex converter
+		
+		while True:
 
+			msg = self.bus.recv()
+			for i in range (0, 4):
+				x =arbitation_filter(msg.arbitration_id)
+				self.out[i] = x
+				print self.out
+			print "1"
+				
 		def dec_converter(msb,lsb):
-
 			dec = 256 * msb + (lsb/16) * 16 + lsb//16 
 			return dec
 
 		def tire_temp(tTemp):
-
 			tTemp = tTemp/10 - 100
-
 			return tTemp
 
 		def arbitation_filter(canid):
+			#take first readings per channel and assign them accordingly
 			msb = msg.data[0]
 			lsb = msg.data[1]
-			switcher = {
-			1204: tire_temp(dec_converter(msb, lsb)),
-			1205: tire_temp(dec_converter(msb, lsb)),
-			1206: tire_temp(dec_converter(msb, lsb)),
-			1207: tire_temp(dec_converter(msb, lsb))
-			}
+			# switcher = {
+			# 1204: tire_temp(dec_converter(msb, lsb)),
+			# 1205: tire_temp(dec_converter(msb, lsb)),
+			# 1206: tire_temp(dec_converter(msb, lsb)),
+			# 1207: tire_temp(dec_converter(msb, lsb))
+			# }
+
+			#Switcher for tire temperature (1200, 1216)
+			switcher = {}
+			for i in range(1204, 1207):
+				switcher[i] = tire_temp(dec_converter(msb, lsb))
+			
 			out = {canid: switcher.get(canid, "nothing")}
 			return out
 			
+		def ecu_arbitation_filter(canid):
+			pass
 
-		while True:
 
-			msg = self.bus.recv()
-			#can_id = msg.can_id
-			#msb = msg.data[0]
-			#lsb = msg.data[1]
-			# Tire temp conversion
-			
-			for i in range (0, 4):
-			
-				x =arbitation_filter(msg.arbitration_id)
-				self.out[i] = x
-				print self.out
-			#print type(msg.arbitration_id)
-			# if msg.arbitration_id == 1204:
-			# 	x = tire_temp(dec_converter(msb, lsb))
 
-				 
-			#print self.x
 
 			#time.sleep(.05)
 
