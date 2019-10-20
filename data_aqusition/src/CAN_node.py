5c9e9e31c08804f9e8b50474d75da8e6a40f6167
farrukh-e <46006626+farrukh-e@users.noreply.github.com>
2019-10-19 22:06:41 -0400

    Add files via upload

--- data_aqusition/src/CAN_node.py
+++ data_aqusition/src/CAN_node.py
@@ -18,7 +18,7 @@ class pyCAN:
 
 		self.bus = can.interface.Bus(channel = self.channel, bustype = self.bustype, bitrate = self.bitrate)
 		self.buffer = can.BufferedReader()
-
+		self.out = [0,0,0,0]
 		rospy.init_node('driver', anonymous=True)
 		#can_pub =  rospy.Publisher("can_bus", Int16, queue_size = 100)
 
@@ -30,22 +30,42 @@ class pyCAN:
 		def tire_temp(tTemp):
 
 			tTemp = tTemp/10 - 100
+
 			return tTemp
+
+		def arbitation_filter(canid):
+			msb = msg.data[0]
+			lsb = msg.data[1]
+			switcher = {
+			1204: tire_temp(dec_converter(msb, lsb)),
+			1205: tire_temp(dec_converter(msb, lsb)),
+			1206: tire_temp(dec_converter(msb, lsb)),
+			1207: tire_temp(dec_converter(msb, lsb))
+			}
+			out = {canid: switcher.get(canid, "nothing")}
+			return out
+			
 
 		while True:
 
 			msg = self.bus.recv()
 			#can_id = msg.can_id
-			msb = msg.data[0]
-			if
-			lsb = msg.data[1]
+			#msb = msg.data[0]
+			#lsb = msg.data[1]
 			# Tire temp conversion
-
 			
+			for i in range (0, 4):
+			
+				x =arbitation_filter(msg.arbitration_id)
+				self.out[i] = x
+				print self.out
+			#print type(msg.arbitration_id)
 			# if msg.arbitration_id == 1204:
 			# 	x = tire_temp(dec_converter(msb, lsb))
-			switch()
-				print(x)
+
+				 
+			#print self.x
+
 			#time.sleep(.05)
 
 
